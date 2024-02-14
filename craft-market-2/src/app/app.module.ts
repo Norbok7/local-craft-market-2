248679d8f1d445c14c4879f279ce122e489f49b0
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-02-14 14:10:29 -0600

    fixed api issues, testing crud on front end, need to test update/create/delete

--- craft-market-2/src/app/app.module.ts
+++ craft-market-2/src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { HttpClientModule, HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';import { UserProfileComponent } from './user/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    CommonModule,
+
+
+
   ],
   providers: [
     provideClientHydration(),
--- craft-market-2/src/app/user/user-profile/user-profile.component.ts
+++ craft-market-2/src/app/user/user-profile/user-profile.component.ts
@@ -4,20 +4,52 @@ import { User } from '../user.model';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  standalone: true,
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css'],
-  imports: [CommonModule, ]
+  styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
   users: User[] | undefined;
+  newUser: User = { username: '', password: '', email: '', user_type: '' };
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
     this.userService.getUsers().subscribe(users => {
       this.users = users;
     });
+  }
+
+  createUser(): void {
+    this.userService.createUser(this.newUser).subscribe(newUser => {
+      console.log('User created:', newUser);
+      this.getUsers();
+      this.newUser = { username: '', password: '', email: '', user_type: '' }; // Reset new user object
+    });
+  }
+
+  updateUser(): void {
+    const userToUpdate = this.users ? this.users[0] : null; // Get the first user (if available)
+    if (userToUpdate) {
+      userToUpdate.username = 'updatedUsername'; // Example: update username
+      this.userService.updateUser(userToUpdate).subscribe(updatedUser => {
+        console.log('User updated:', updatedUser);
+        this.getUsers();
+      });
+    }
+  }
+
+  deleteUser(): void {
+    const userIdToDelete = this.users ? this.users[0]?.id : null; // Get the ID of the first user (if available)
+    if (userIdToDelete) {
+      this.userService.deleteUser(userIdToDelete).subscribe(() => {
+        console.log('User deleted successfully');
+        this.getUsers();
+      });
+    }
   }
 }
