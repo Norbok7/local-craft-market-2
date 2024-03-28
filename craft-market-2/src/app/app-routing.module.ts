7b4b9e7987ae62c9dc139a91a0e918120680e97c
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-03-28 13:27:45 -0500

    route update removed home as products will be home

--- craft-market-2/src/app/app-routing.module.ts
+++ craft-market-2/src/app/app-routing.module.ts
@@ -8,14 +8,12 @@ import { ProductListComponent } from './product/product-list/product-list.compon
 import { ProductDetailsComponent } from './product/product-details/product-details.component';
 import { OrderListComponent } from './order/order-history/order-list.component';
 import { ReviewDetailsComponent } from './review/review-details/review-details.component';
-import { HomeComponent } from './shared/home/home.component'; // Import HomeComponent
 import { AuthGuard } from './shared/login/auth.guard';
 import { UserLoginComponent } from './shared/login/user-login/user-login.component';
 import { CartComponent } from './product/cart/cart.component';
 import { ArtisanLoginComponent } from './shared/login/artisan-login/artisan-login.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
   { path: 'users/:id', component: UserProfileComponent, canActivate: [AuthGuard] },
   { path: 'users/:id/orders', component: UserOrdersComponent, canActivate: [AuthGuard] },
   { path: 'artisan-profile', component: ArtisanProfileComponent, canActivate: [AuthGuard] },
@@ -24,17 +22,16 @@ const routes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
   { path: 'orders', component: OrderListComponent, canActivate: [AuthGuard] },
-  { path: 'orders/:id', component: OrderListComponent, canActivate: [AuthGuard] }, // Update to OrderListComponent
-  { path: 'orders/:id/:totalAmount', component: OrderListComponent, canActivate: [AuthGuard] }, // Update to OrderListComponent
+  { path: 'orders/:id', component: OrderListComponent, canActivate: [AuthGuard] },
+  { path: 'orders/:id/:totalAmount', component: OrderListComponent, canActivate: [AuthGuard] },
   { path: 'reviews/:id', component: ReviewDetailsComponent, canActivate: [AuthGuard] },
   { path: 'artisans/:id/products', component: ArtisanProductsComponent, canActivate: [AuthGuard] },
   { path: 'login/user', component: UserLoginComponent },
   { path: 'cart', component: CartComponent },
   { path: 'artisan-login', component: ArtisanLoginComponent },
 
-  // { path: 'users/current', component: UserProfileComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home' }
+  { path: '', redirectTo: '/products', pathMatch: 'full' }, // Redirect to products page
+  { path: '**', redirectTo: '/products' } // Handle invalid routes by redirecting to products page
 ];
 
 @NgModule({
